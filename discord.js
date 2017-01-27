a907eb2a92d821286e755d154e3a31a4d113e951
Libter <libter.pl@gmail.com>
2017-01-27 22:39:28 +0100

    Better logging

--- discord.js
+++ discord.js
@@ -1,8 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+function botLog(msg) {
+    console.log(`Bot: ${msg}`)
+}
+
+function userLog(nick, msg) {
+    console.log(`User ${nick}: ${msg}`);
+}
+
 client.on('ready', () => {
-	console.log('I am ready!');
+	botLog('Ready!');
 	client.user.setGame('służę pomocą!');
 	client.user.setAvatar('./avatar3.png');
 });
@@ -12,30 +20,37 @@ client.on('message', message => {
 
     let command = message.content.toLowerCase();
     let nick = message.author.username;
+    let channel = message.channel;
     switch (command) {
         case '.ping':
+            userLog(nick, 'command PING');
             message.reply('pong!');
             break;
         case '.pong':
+            userLog(nick, 'command PONG');
             message.reply('ping!');
             break;
         case 'hi kuvu!':
+            userLog(nick, 'command HI_KUVU');
             channel.sendMessage('Hi ' + message.author.username + '!');
             break;
         case '#reklama':
-            console.log(`User ${nick} use ad command.`);
+            userLog(nick, 'command REKLAMA');
             message.delete();
             channel.sendMessage('Zapraszam do zapoznania się z innymi projektami mojego stwórcy!\nStrona www: https://kuvus.pl');
             break;
         case '.pomoc':
         case '.help':
+            userLog(nick, 'command HELP');
             message.author.sendMessage('Oto lista komend:\n```.pomoc - wyświetla pomoc\n.ping - wysyła wiadomość o treści ping\n.avatar - wysyła adres URL do twojego avatara\n.servers - Lista serwerów używających kuvuBota```');
             message.reply('lista komend została wysłana na PW!');
             break;
         case '.servers':
+            userLog(nick, 'command SERVERS');
             message.reply('serwery, które używają kuvuBota: \n' + '```' + client.guilds.map(r => '\n' + r.name + ' (#' + r.id + ')') + '```');
         default:
             if (command.startsWith('.avatar')) {
+                userLog(nick, 'command AVATAR');
                 let args = message.content.split(" ", 2);
                 let cmd = args[0];
                 let ment = args[1];
