d63092be85b5d61040cb8cb8de4d2412404c5e80
kuvuś <kuba17ism@tuta.io>
2017-04-01 21:13:42 +0200

    Update config.js

--- kuvuBot/kuvuBot/config.js
+++ kuvuBot/kuvuBot/config.js
@@ -1,6 +1,6 @@
 var config = {};
 config.apikeys = {
-    "discord_token": "Mjc2ODAxMTQwMDA2NTg0MzIw.C7_t-g.m--TqOa-9aUfhBzaDVK2W5zTCoI",
+    "discord_token": "",
     "cleverbot_api_key": ""
 };
 
